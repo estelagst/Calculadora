d50692b185cc34534543a9e3af7472a28673e26c
Estela <gonzalesestelinha@gmail.com>
2021-04-05 12:03:08 -0300

    calculadora

---
+++ calculadora/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/estelagst/pen/oNBgMKy](https://codepen.io/estelagst/pen/oNBgMKy).
+
+
---
+++ calculadora/src/script.js
@@ -0,0 +1,29 @@
+var operacao = prompt("Qual operação você deseja fazer? Digite 1 para  soma, 2 para subtração, 3 para multiplicação e 4 para divisão:   ");
+var primeiroValor = parseFloat(prompt("Digite o primeiro valor: "));
+var segundoValor = parseFloat(prompt("Digite o segundo valor: "));
+var resultado = 0;
+var operador = '';
+if (operacao == 1) {
+  resultado = primeiroValor + segundoValor;
+  operador = ' + ';
+} else if (operacao ==2) {
+  resultado = primeiroValor - segundoValor;
+  operador = " - ";
+} else if (operacao == 3) {
+  resultado = primeiroValor * segundoValor;
+  operador = " x ";
+} else if (operacao == 4) {
+  resultado = primeiroValor / segundoValor;
+  operador = " / ";
+}  else {
+  alert("Opção inválida");
+}
+
+ document.write("<h2>" + primeiroValor + operador + segundoValor + " = " + resultado.toFixed(2) + "</h2>");
+
+
+
+
+//if = se
+// else = senão
+// else if = senão se 
